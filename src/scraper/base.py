f9478c725b06217fe62b1262f8fa12a494c59165
chmonesmith <chm24@bu.edu>
2025-06-30 13:22:40 -0400

    fix(scraper): Rename Scraper to BaseScraper to fix ImportError

--- src/scraper/base.py
+++ src/scraper/base.py
@@ -1,21 +1,26 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Iterator
 # We will define the Job data structure in a separate file
-# from .models import Job 
+from .models import Job 
 
-class Scraper(ABC):
+class BaseScraper(ABC):
     """
     Abstract base class for all scrapers.
     Defines the common interface for scraping job data.
     """
+    def __init__(self, driver):
+        self.driver = driver
 
     @abstractmethod
-    def scrape(self) -> List: # List[Job]
+    def scrape(self, url: str) -> Iterator[Job]:
         """
         The core method to perform the scraping.
         This should be implemented by all concrete scraper classes.
 
+        Args:
+            url: The URL to scrape.
+
         Returns:
-            A list of Job data objects.
+            An iterator of Job data objects.
         """
         pass 
